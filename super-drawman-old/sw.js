1493d75d0de3816d72ba507fae635b8ff593f4cc
3damp <amp206@gmail.com>
2024-08-27 09:18:47 +0100

    pwa test

---
+++ super-drawman-old/sw.js
@@ -0,0 +1,13 @@
+var GHPATH = '/unity-prototypes/super-drawman-old';
+var APP_PREFIX = 'sdmo_';
+var VERSION = 'v_00';
+
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/loader.js`
+  `${GHPATH}/Build/`,
+  `${GHPATH}/Build/game.data`,
+  `${GHPATH}/Build/game.framework.js`,
+  `${GHPATH}/Build/game.wasm`,
+]
