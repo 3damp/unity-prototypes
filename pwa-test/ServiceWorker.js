d321c00cfffdce89b225faf0f7eef20616652ac2
3damp <amp206@gmail.com>
2024-08-28 18:57:43 +0100

    update serviceWorker

--- pwa-test/ServiceWorker.js
+++ pwa-test/ServiceWorker.js
@@ -9,7 +9,7 @@ const contentToCache = [
 ];
 
 self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
+    console.log('[Service Worker] Install', cacheName);
     
     e.waitUntil((async function () {
       caches.keys().then((cacheNames) => {
@@ -24,7 +24,7 @@ self.addEventListener('install', function (e) {
       })
 
       const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
+      console.log('[Service Worker] Caching all: app shell and content', cacheName);
       await cache.addAll(contentToCache);
     })());
 });
@@ -32,18 +32,18 @@ self.addEventListener('install', function (e) {
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
       let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`, cacheName);
       if (response) { return response; }
 
       response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`, cacheName);
       cache.put(e.request, response.clone());
       return response;
     })());
 });
 self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activate');
+  console.log('[Service Worker] Activate', cacheName);
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
